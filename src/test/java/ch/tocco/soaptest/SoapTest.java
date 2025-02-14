38045bfca62b7bea7159982bff81df7619fa9027
Adrian Schmid <aschmid@tocco.ch>
2025-02-14 18:41:46 +0100

    add implementation with apache cxf

--- src/test/java/ch/tocco/soaptest/SoapTest.java
+++ src/test/java/ch/tocco/soaptest/SoapTest.java
@@ -2,30 +2,31 @@ package ch.tocco.soaptest;
 
 import ch.tocco.soaptest.soap.B2BService;
 import ch.tocco.soaptest.soap.B2BService_Service;
+import jakarta.xml.ws.BindingProvider;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ResourceLoader;
 
 import java.net.URI;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class SoapTest {
-    @Autowired
-    private ResourceLoader resourceLoader;
 
     @Test
     void testPing() {
         try {
-            assertTrue(resourceLoader.getResource("classpath:B2BService.xml").exists());
-            assertTrue(resourceLoader.getResource("classpath:wsit-client.xml").exists());
-
             URI uri = URI.create("https://ebill-ki.postfinance.ch/B2BService/B2BService.svc?singleWsdl");
-            B2BService port = new B2BService_Service(uri.toURL()).getUserNamePassword();
+            B2BService port = new B2BService_Service(uri.toURL(), B2BService_Service.SERVICE).getUserNamePassword();
 
-            String result = port.executePing("41101000001294361", null, null, null);
+
+            Map<String, Object> requestContext = ((BindingProvider) port).getRequestContext();
+            requestContext.put("security.username", "username");
+            requestContext.put("security.password", "password");
+
+            String result = port.executePing("41101000001294361", "", null, null);
 
             assertEquals("41101000001294361", result);
         } catch (Exception e) {
